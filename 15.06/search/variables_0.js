9a185a061d6a67365c5680432f8621b0ac88208f
monolith <monolith@ultimaker.com>
2015-07-15 06:25:07 +0200

    Automated Documentation Generation

--- 15.06/search/variables_0.js
+++ 15.06/search/variables_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['activetoolchanged',['activeToolChanged',['../classUM_1_1Controller_1_1Controller.html#ac1ee8ed6e11de08b40546f3e87fd3566',1,'UM::Controller::Controller']]],
-  ['activeviewchanged',['activeViewChanged',['../classUM_1_1Controller_1_1Controller.html#ae67d4ba75051c32e0f2a7f7e1cd9ff56',1,'UM::Controller::Controller']]]
+  ['activeviewchanged',['activeViewChanged',['../classUM_1_1Controller_1_1Controller.html#ae67d4ba75051c32e0f2a7f7e1cd9ff56',1,'UM::Controller::Controller']]],
+  ['applicationshuttingdown',['applicationShuttingDown',['../classUM_1_1Application_1_1Application.html#a072a2eb260ea9c4da8c13a6b97522847',1,'UM::Application::Application']]]
 ];
